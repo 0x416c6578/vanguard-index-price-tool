b98086d99d45a580e88e259c5b2e51b4252bd4d1
Alex Underwood <14030169+0x416c6578@users.noreply.github.com>
2025-08-31 20:09:40 +0200

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module 0x416c6578.github.io/vanguard-index-price-tool
+
+go 1.25.0
---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"sync"
+)
+
+// struct for incoming data, containing only used fields
+type data struct {
+	NavPrice struct {
+		Value string `json:"value"`
+	} `json:"navPrice"`
+}
+
+type vgfund struct {
+	Name string
+	URL  string
+}
+
+var funds = []vgfund{
+	{Name: "LifeStrategy 20% Equity", URL: "https://www.vanguardinvestor.co.uk/api/funds/vanguard-lifestrategy-20-equity-fund-gbp-gross-accumulation-shares"},
+	{Name: "LifeStrategy 40% Equity", URL: "https://www.vanguardinvestor.co.uk/api/funds/vanguard-lifestrategy-40-equity-fund-accumulation-shares"},
+	{Name: "LifeStrategy 60% Equity", URL: "https://www.vanguardinvestor.co.uk/api/funds/vanguard-lifestrategy-60-equity-fund-accumulation-shares"},
+	{Name: "LifeStrategy 80% Equity", URL: "https://www.vanguardinvestor.co.uk/api/funds/vanguard-lifestrategy-80-equity-fund-accumulation-shares"},
+	{Name: "LifeStrategy 100% Equity", URL: "https://www.vanguardinvestor.co.uk/api/funds/vanguard-lifestrategy-100-equity-fund-accumulation-shares"},
+}
+
+func main() {
+	fmt.Println("Vanguard Price Retrieval")
+
+	wg := &sync.WaitGroup{}
+	wg.Add(len(funds))
+
+	for _, fund := range funds {
+		go retrieve(fund, wg)
+	}
+
+	wg.Wait()
+}
+
+func retrieve(fund vgfund, wg *sync.WaitGroup) {
+	resp, err := http.Get(fund.URL)
+	if err != nil {
+		fmt.Println("Couldn't retrieve %s", fund.URL)
+		return
+	}
+	defer resp.Body.Close()
+
+	var d data
+	if err := json.NewDecoder(resp.Body).Decode(&d); err != nil {
+		return
+	}
+	fmt.Printf("%s: %s\n", fund.Name, d.NavPrice.Value)
+
+	wg.Done()
+}
