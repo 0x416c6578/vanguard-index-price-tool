909d3f6400ab2ff072ebe414fc495ef168b57246
Alex Underwood <14030169+0x416c6578@users.noreply.github.com>
2025-09-02 20:40:11 +0200

    more idiomatic go bits

--- main.go
+++ main.go
@@ -2,82 +2,105 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"net/http"
 	"os"
-	"slices"
 	"strconv"
 	"strings"
+	"time"
 )
 
-// struct for incoming data, containing only used fields
-type data struct {
-	NavPrice struct {
-		Value string `json:"value"`
-	} `json:"navPrice"`
-}
-
 type vgfund struct {
-	Name string
-	URL  string
-	ID   string
+	Name  string
+	URL   string
+	Price float64
 }
 
-type vgfundprice struct {
+type result struct {
 	vgfund
-	price float64
+	err error
 }
 
-var availableFunds = []vgfund{
-	{Name: "LifeStrategy 20% Equity", ID: "ls20", URL: "https://www.vanguardinvestor.co.uk/api/funds/vanguard-lifestrategy-20-equity-fund-gbp-gross-accumulation-shares"},
-	{Name: "LifeStrategy 40% Equity", ID: "ls40", URL: "https://www.vanguardinvestor.co.uk/api/funds/vanguard-lifestrategy-40-equity-fund-accumulation-shares"},
-	{Name: "LifeStrategy 60% Equity", ID: "ls60", URL: "https://www.vanguardinvestor.co.uk/api/funds/vanguard-lifestrategy-60-equity-fund-accumulation-shares"},
-	{Name: "LifeStrategy 80% Equity", ID: "ls80", URL: "https://www.vanguardinvestor.co.uk/api/funds/vanguard-lifestrategy-80-equity-fund-accumulation-shares"},
-	{Name: "LifeStrategy 100% Equity", ID: "ls100", URL: "https://www.vanguardinvestor.co.uk/api/funds/vanguard-lifestrategy-100-equity-fund-accumulation-shares"},
+var availableFunds = map[string]vgfund{
+	"ls20":  {Name: "LifeStrategy 20% Equity", URL: "https://www.vanguardinvestor.co.uk/api/funds/vanguard-lifestrategy-20-equity-fund-gbp-gross-accumulation-shares"},
+	"ls40":  {Name: "LifeStrategy 40% Equity", URL: "https://www.vanguardinvestor.co.uk/api/funds/vanguard-lifestrategy-40-equity-fund-accumulation-shares"},
+	"ls60":  {Name: "LifeStrategy 60% Equity", URL: "https://www.vanguardinvestor.co.uk/api/funds/vanguard-lifestrategy-60-equity-fund-accumulation-shares"},
+	"ls80":  {Name: "LifeStrategy 80% Equity", URL: "https://www.vanguardinvestor.co.uk/api/funds/vanguard-lifestrategy-80-equity-fund-accumulation-shares"},
+	"ls100": {Name: "LifeStrategy 100% Equity", URL: "https://www.vanguardinvestor.co.uk/api/funds/vanguard-lifestrategy-100-equity-fund-accumulation-shares"},
 }
 
 func main() {
+	selectedFunds := getSelectedFunds()
+
+	client := &http.Client{Timeout: 10 * time.Second}
+
+	ch := make(chan result, len(selectedFunds))
+	for _, fund := range selectedFunds {
+		go retrieveFundPrice(client, fund, ch)
+	}
+
+	for i := len(selectedFunds); i != 0; i-- {
+		fundInfo := <-ch
+		if fundInfo.err != nil {
+			fmt.Println("Failed to receive fund info for", fundInfo.Name, fundInfo.err)
+			continue
+		}
+
+		fmt.Println(fundInfo.Name, fundInfo.Price)
+	}
+}
+
+func getSelectedFunds() []vgfund {
 	var selectedFunds []vgfund
 
 	if len(os.Args) == 2 {
 		cliFunds := strings.Split(os.Args[1], ",")
 
-		for _, v := range availableFunds {
-			if slices.Contains(cliFunds, v.ID) {
-				selectedFunds = append(selectedFunds, v)
+		for _, v := range cliFunds {
+			fund, ok := availableFunds[v]
+			if ok {
+				selectedFunds = append(selectedFunds, fund)
 			}
 		}
 	} else {
-		selectedFunds = availableFunds
+		for _, v := range availableFunds {
+			selectedFunds = append(selectedFunds, v)
+		}
 	}
-
-	ch := make(chan vgfundprice, len(selectedFunds))
-
-	for _, fund := range selectedFunds {
-		go retrieve(fund, ch)
-	}
-
-	for i := len(selectedFunds); i != 0; i-- {
-		fundPrice := <-ch
-		fmt.Println(fundPrice.Name, fundPrice.price)
-	}
-
+	return selectedFunds
 }
 
-func retrieve(fund vgfund, ch chan vgfundprice) {
-	resp, err := http.Get(fund.URL)
+func retrieveFundPrice(client *http.Client, fund vgfund, ch chan<- result) {
+	resp, err := client.Get(fund.URL)
 	if err != nil {
-		fmt.Println("Couldn't retrieve %s", fund.URL)
+		ch <- result{fund, errors.New("failed to get fund info from URL")}
 		return
 	}
 	defer resp.Body.Close()
 
-	var d data
+	if resp.StatusCode != http.StatusOK {
+		ch <- result{fund, errors.New("bad response from vanguard")}
+	}
+
+	var d struct {
+		NavPrice struct {
+			Value string `json:"value"`
+		} `json:"navPrice"`
+	}
+
 	if err := json.NewDecoder(resp.Body).Decode(&d); err != nil {
+		ch <- result{fund, errors.New("failed to decode response body")}
 		return
 	}
 
-	fundValue, err := strconv.ParseFloat(d.NavPrice.Value, 64)
+	fundPrice, err := strconv.ParseFloat(d.NavPrice.Value, 64)
+	if err != nil {
+		ch <- result{fund, errors.New("failed to read NavPrice value as float")}
+		return
+	}
 
-	ch <- vgfundprice{fund, fundValue}
+	fund.Price = fundPrice
+
+	ch <- result{fund, nil}
 }
